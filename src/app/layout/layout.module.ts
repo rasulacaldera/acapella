8e4dfef2155a52d748bb45176b98a029cd7745a7
rasula <rasula.caldera@gmail.com>
2020-06-19 20:19:27 +0530

    Removing routing module for layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
-    LayoutRoutingModule
+    RouterModule
   ]
 })
 export class LayoutModule { }
