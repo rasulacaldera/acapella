d9585220891524e6b8424ae74d6dcc44bde5d4a2
rasula <rasula.caldera@gmail.com>
2020-06-19 20:16:49 +0530

    Adding layout lazyloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: "", component: LayoutComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  {
+    path: "home",
+    component: LayoutComponent,
+    loadChildren: './home/home.module#HomeModule'
+  },
   { path: "**", redirectTo: "", pathMatch: "full" }
 ];
 
