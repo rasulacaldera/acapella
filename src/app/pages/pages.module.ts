221a2f9c49ae1b4927b992b9aa4a31575e39eb15
rasula <rasula.caldera@gmail.com>
2020-06-30 18:24:20 +0530

    adding contribution btn panel for home

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,9 +6,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomeCarouselComponent } from './home/home-carousel/home-carousel.component';
+import { ContributeBtnPanelComponent } from './home/contribute-btn-panel/contribute-btn-panel.component';
 
 @NgModule({
-  declarations: [HomeComponent, AboutUsComponent, HomeCarouselComponent],
+  declarations: [
+    HomeComponent,
+    AboutUsComponent,
+    HomeCarouselComponent,
+    ContributeBtnPanelComponent
+  ],
   imports: [CommonModule, PagesRoutingModule, SharedModule]
 })
 export class PagesModule {}
