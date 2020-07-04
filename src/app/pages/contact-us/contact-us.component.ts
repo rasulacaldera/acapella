2ac835df9b177029072595ccb629df9efd54a5d8
chaya <chayyya990gmail.com>
2020-07-04 13:55:46 +0530

    Adding contact form with email send

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
@@ -9,4 +10,23 @@ export class ContactUsComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'gmail',
+        'template_1Sbs6Jqq',
+        e.target as HTMLFormElement,
+        'user_pSEgpugwfCXld8vjOSJaP'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  }
 }
