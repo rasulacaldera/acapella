7c56ae023ea47e7753ace33006c1f5bb68d25930
rasula <rasula.caldera@gmail.com>
2020-11-21 21:27:16 +0530

    Adding configurable video URL library

--- src/app/pages/home/recent-photos/recent-photos.component.ts
+++ src/app/pages/home/recent-photos/recent-photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'recent-photos',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recent-photos.component.scss']
 })
 export class RecentPhotosComponent implements OnInit {
-  constructor() {}
+  videoSources: any = [
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw',
+    'https://www.youtube.com/embed/7GjOOyBoELw'
+  ];
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit() {}
+
+  getSafeUrl(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
