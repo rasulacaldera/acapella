341d8b6836808e5a59df079f8d75f8c4af4cfe0f
rasula <rasula.caldera@gmail.com>
2020-11-21 18:45:36 +0530

    adding skip to temp page in dev

--- src/app/layout/under-construction/under-construction.component.ts
+++ src/app/layout/under-construction/under-construction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-under-construction',
@@ -12,6 +13,8 @@ export class UnderConstructionComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    !environment.production ? (window.location.href = '/home') : null;
+
     setInterval(() => {
       // Get todays date and time
       var now = new Date().getTime();
