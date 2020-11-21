d01b8ab036d8430c76a16ece44d6e21d3579945d
rasula <rasula.caldera@gmail.com>
2020-11-21 15:48:30 +0530

    under contruction page

---
+++ src/app/layout/under-construction/under-construction.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-under-construction',
+  templateUrl: './under-construction.component.html',
+  styleUrls: ['./under-construction.component.scss']
+})
+export class UnderConstructionComponent implements OnInit {
+  countDownDate = new Date('Jan 1, 2021 00:00:00').getTime();
+  countDownDisplay = 'Calculating...';
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = this.countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in an element with id="demo"
+      this.countDownDisplay =
+        days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+    }, 1000);
+  }
+}
