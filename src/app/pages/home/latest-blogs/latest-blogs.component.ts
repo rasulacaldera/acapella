856b4da0934239ed6aed4e6ca147c8386fa71538
rasula <rasula.caldera@gmail.com>
2021-01-14 17:07:54 +0530

    Adding loader to latest blogs

--- src/app/pages/home/latest-blogs/latest-blogs.component.ts
+++ src/app/pages/home/latest-blogs/latest-blogs.component.ts
@@ -9,10 +9,12 @@ import { BlogService } from 'src/services/blog.service';
 })
 export class LatestBlogsComponent implements OnInit {
   blogPosts: any = [];
+  loading: boolean = false;
 
   constructor(private blogService: BlogService) {}
 
   ngOnInit() {
+    this.loading = true;
     this.blogService.getLatestBlogs(3).subscribe(res => {
       this.blogPosts = res;
       this.blogPosts = this.blogPosts.map(blog => {
@@ -23,6 +25,7 @@ export class LatestBlogsComponent implements OnInit {
           summary: blog.summary.trim().slice(0, 500) + '...'
         };
       });
+      this.loading = false;
     });
   }
 }
