52baab9ac6c2464b88531ade744edf28289ba017
rasula <rasula.caldera@gmail.com>
2021-01-14 14:05:46 +0530

    footer navigation set

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onClick($event, url) {
+    $event.preventDefault();
+    window.open(url, '_blank');
   }
-
 }
