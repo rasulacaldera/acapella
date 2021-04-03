9125a488d600576814e6ae7bf59f74a18897444a
rasula <rasula.caldera@gmail.com>
2021-04-03 10:40:16 +0530

    removing under construction page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
-import { UnderConstructionComponent } from './layout/under-construction/under-construction.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '',
-    component: UnderConstructionComponent,
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: '',
     component: LayoutComponent,
