c6cf29494cd284478639bcdc956c0d90ea7d09af
rasula <rasula.caldera@gmail.com>
2021-04-03 15:39:54 +0530

    adding btn action and removing nav options

--- src/app/pages/home/home-carousel/home-carousel.component.ts
+++ src/app/pages/home/home-carousel/home-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home-carousel',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-carousel.component.scss']
 })
 export class HomeCarouselComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  redirect(url) {
+    this.router.navigate([url]);
+  }
 }
